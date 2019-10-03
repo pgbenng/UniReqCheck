319e66781dd48603d364629121b0c082313b9dbf
Ta Thi Phuong Ha <macair@MacAirs-MacBook-Air.local>
2019-10-03 16:39:03 -0700

    designed admin page

--- app/components/Admin/index.jsx
+++ app/components/Admin/index.jsx
@@ -1,49 +1,69 @@
-import React from 'react';
-import axios from 'axios'; 
+import React from "react";
+import axios from "axios";
 
 export default class Admin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        uniName: '',
-        req: ''
-    }
+      uniName: "",
+      req: "",
+    };
   }
 
-  componentDidMount(){
-    
-  }
+  componentDidMount() {}
 
   handleAddUniName(e) {
     this.setState({
-        uniName: e.target.value
-    })
+      uniName: e.target.value
+    });
   }
 
   handleAddReq(e) {
     this.setState({
-        req: e.target.value
-    })
-
+      req: e.target.value
+    });
   }
 
   handleSubmitReq() {
-    axios.post('/uni/req', {
+    axios
+      .post("/uni/req", {
         uniName: this.state.uniName,
         req: this.state.req
-    })
-    .then (res => {
-        console.log(res)
-    })
+      })
+      .then(res => {
+        console.log(res);
+        alert('submitted')
+      });
   }
   render() {
     return (
-      <div>
-        <input type="text" onChange={this.handleAddUniName.bind(this)} placeholder="Uni name"></input>
-        <input type="text" onChange={this.handleAddReq.bind(this)} placeholder="Add reqs"></input>
-        <button onClick={this.handleSubmitReq.bind(this)}></button>
+      <div id='container'>
+        <div className="row">
+          <div className="col-md-12 inputboxes" >
+            <h1 id="admintitle"> Post-Secondary Requirements Admin Page</h1>
+            <input
+              id="uniname"
+              type="text"
+              onChange={this.handleAddUniName.bind(this)}
+              placeholder="Uni name"
+            ></input>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+              <textarea
+              
+              onChange={this.handleAddReq.bind(this)}
+              placeholder="Add reqs"
+              rows="10"
+              cols="100"
+              
+            ></textarea>
+              </div>
+            </div>
+            <button onClick={this.handleSubmitReq.bind(this)}> Submit</button>
+          
+        </div>
       </div>
-    )
+    );
   }
 }
-

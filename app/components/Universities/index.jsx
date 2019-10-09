cbd27e3a304b24f7beb099ff175e39b1f3f7b634
Ta Thi Phuong Ha <macair@MacAirs-MacBook-Air.local>
2019-10-08 19:27:24 -0700

    mobile friendlier

--- app/components/Universities/index.jsx
+++ app/components/Universities/index.jsx
@@ -4,43 +4,40 @@ import ReactMarkdown from "react-markdown";
 import Navbar from "../Navbar/index.jsx";
 
 export default class Universities extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        req : ""
-        
-        
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      req: ""
+    };
+  }
 
-    componentDidMount() {
-      axios.get("/uni/reqSearch",{
-        params:{
-          uniName : this.props.params.uniName,
-          faculty : this.props.params.faculty
+  componentDidMount() {
+    axios
+      .get("/uni/reqSearch", {
+        params: {
+          uniName: this.props.params.uniName,
+          faculty: this.props.params.faculty
         }
-      }).then(res=>{
-        console.log(res.data)
+      })
+      .then(res => {
+        console.log(res.data);
         this.setState({
           req: res.data.reqs
-        })
-      })
+        });
+      });
+  }
 
-    }
-
-    
-    render() {
-        return (
-          
-        <div className = 'univer'>
-          <Navbar backgroundcolor= 'black' textcolor='white'/>
-          <div id="reqbody">
-           <ReactMarkdown source = {this.state.req}/>
-           </div>
+  render() {
+    return (
+      <div className="univer row">
+        <div className="col-md-12"></div>
+        <Navbar backgroundcolor="black" textcolor="white" />
+        <div id="reqbody" className="row">
+          <div className="col-md-12">
+            <ReactMarkdown source={this.state.req} />
+          </div>
         </div>
-        
-        
-
-        )}
+      </div>
+    );
+  }
 }
-
